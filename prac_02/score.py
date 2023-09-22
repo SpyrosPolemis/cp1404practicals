b71a6e5687f35751f5e831d7ad0bc4a71025f185
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-09-22 11:08:16 +1000

    Refactor score.py to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,21 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+
+def main():
+    score = float(input("Enter score: "))
+    print(evaluate_score(score))
+
+
+def evaluate_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
