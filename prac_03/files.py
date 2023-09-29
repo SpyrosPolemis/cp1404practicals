3619d79d8ffa46bda269bad83782c5a3e485dc71
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-09-29 11:44:26 +1000

    Complete question 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -10,5 +10,13 @@ def question_2():
         print(f"Your name is {name}")
 
 
+def question_3():
+    with open("numbers.txt") as in_file:
+        (first_number) = in_file.readline().strip()
+        (second_number) = in_file.readline().strip()
+        total = int(first_number) + int(second_number)
+        print(total)
+
 question_1()
 question_2()
+question_3()
