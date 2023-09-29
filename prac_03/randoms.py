d302d15afb55181cab147b10d0796311e4005ae5
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-09-29 10:44:12 +1000

    Add code to produce random number between 1 and 100

---
+++ prac_03/randoms.py
@@ -0,0 +1,7 @@
+# 1. 17, Lowest being 5, highest being 20
+# 2 . 9, Lowest being 3, highest being 9
+# 3. 5.442028030359838, lowest being 2.5, highest being 9.999999999999999
+import random
+
+random_number = random.randint(1, 100)
+print(random_number)
