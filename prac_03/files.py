ae25305ceb43dd9b600f467ca0836f5e604af74a
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-09-29 11:30:57 +1000

    Complete question 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,4 +3,12 @@ def question_1():
     with open("name.txt", "w") as out_file:
         print(name, file=out_file)
 
+
+def question_2():
+    with open("name.txt") as in_file:
+        name = in_file.read()
+        print(f"Your name is {name}")
+
+
 question_1()
+question_2()
