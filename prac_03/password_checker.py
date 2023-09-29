4f567c7085025134985373f631b8326d6f8ce446
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-09-29 12:36:55 +1000

    Check if SPECIAL_CHARS_REQUIRED and if True check that there is at least 1 special character

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -45,11 +45,9 @@ def is_valid_password(password):
             count_special += 1
     if count_lower == 0 or count_upper == 0 or count_digit == 0:
         return False
-
-    # TODO: if special characters are required, then check the count of those
-    # and return False if it's zero
-
-    # if we get here (without returning False), then the password must be valid
+    if SPECIAL_CHARS_REQUIRED:
+        if count_special == 0:
+            return False
     return True
 
 
