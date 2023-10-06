06f7ec7ae83771a23af2e3c0916fdf1ca4795847
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-10-06 12:13:04 +1000

    Add list comprehension to generate list of last names if full name is longer than 9 letters

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -36,11 +36,11 @@ almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 numbers = [int(number) for number in almost_numbers]
 print(numbers)
 
-# TODO: list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
 numbers_greater_than_nine = [number for number in numbers if number > 9]
 print(numbers_greater_than_nine)
 
-# TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
 # the result should be: 'Harlem, Hendrix, Lovelace'
+big_last_names = ", ".join([name.split()[1] for name in full_names if len(name) > 11])
+print(big_last_names)
