68daec3318304738724bcef8231a7d1024789c99
SpyrosPolemis <Spyridon.polemis@my.jcu.edu.au>
2023-10-06 11:44:17 +1000

    Add woefully inadequate security checker

--- prac_04/list_excercises.py
+++ prac_04/list_excercises.py
@@ -16,3 +16,9 @@ print(sum(numbers) / NUMBER_OF_NUMBERS)
 
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Username: ")
+if username in usernames:
+    print("Access granted.")
+else:
+    print("Access denied")
