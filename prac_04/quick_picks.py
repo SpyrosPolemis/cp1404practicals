99aaf385cac65cf713d98485c096dafc47eab9b0
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-10-08 23:15:14 +1000

    Add NUMBERS_PER_LINE constant to remove magic numbers from program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,17 +5,18 @@ import random
 
 MINIMUM_NUMBER = 1
 MAXIMUM_NUMBER = 45
+NUMBERS_PER_LINE = 6
 
 number_of_quick_picks = int(input("How many quick picks? "))
 random_numbers = []
 for i in range(number_of_quick_picks):
-    for j in range(6):
+    for j in range(NUMBERS_PER_LINE):
         random_number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
         while random_number in random_numbers:
             random_number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
         random_numbers.append(random_number)
     random_numbers.sort()
     for number in random_numbers:
-        print(number, end=" ")
+        print(f"{number:2}", end=" ")
     print()
     random_numbers = []
