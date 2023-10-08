f505e4dec669a3eaa0bdc3dfc080ad101ab585f5
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-10-08 23:19:09 +1000

    Change variable name random_numbers to quick_pick for more clarity

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,15 +8,15 @@ MAXIMUM_NUMBER = 45
 NUMBERS_PER_LINE = 6
 
 number_of_quick_picks = int(input("How many quick picks? "))
-random_numbers = []
+quick_pick = []  # doesn't really sound like a list but random_numbers etc. also feels wrong
 for i in range(number_of_quick_picks):
     for j in range(NUMBERS_PER_LINE):
         random_number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
-        while random_number in random_numbers:
+        while random_number in quick_pick:
             random_number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
-        random_numbers.append(random_number)
-    random_numbers.sort()
-    for number in random_numbers:
+        quick_pick.append(random_number)
+    quick_pick.sort()
+    for number in quick_pick:
         print(f"{number:2}", end=" ")
     print()
-    random_numbers = []
+    quick_pick = []
