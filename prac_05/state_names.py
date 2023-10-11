b6f8992273d64abb2c83b0d48149a7fc4926d8fa
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-10-11 22:26:14 +1000

    Refactor to use exception EAFP instead of LBYL error checking

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,10 +9,21 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
 for code, name in CODE_TO_NAME.items():
     print(f"{code:3} is {name}")
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+# state_code = input("Enter short state: ").upper()
+# while state_code != "":
+#     if state_code in CODE_TO_NAME:
+#         print(state_code, "is", CODE_TO_NAME[state_code])
+#     else:
+#         print("Invalid short state")
+#     state_code = input("Enter short state: ").upper()
+
+is_valid_input = False
+while not is_valid_input:
+    try:
+        state_code = input("Enter short state: ").upper()
+        while state_code != "":
+            print(state_code, "is", CODE_TO_NAME[state_code])
+            state_code = input("Enter short state: ").upper()
+        is_valid_input = True
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
