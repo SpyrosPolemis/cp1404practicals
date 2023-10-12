a0a7e5b108e8efd744ce0876dad0250bb84f05ae
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-10-12 22:43:21 +1000

    Add actual time taken to complete code to docstring

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,7 +1,7 @@
 """
 Word occurrences
 Estimate: 30 minutes
-Actual:
+Actual: 27 minutes
 """
 
 text = input("Text: ")
@@ -17,4 +17,3 @@ words_and_counts.sort()
 longest_word_length = max((len(word) for word in word_to_number_of_word))
 for word_and_count in words_and_counts:
     print(f"{word_and_count[0]:{longest_word_length}} : {word_and_count[1]}")
-
