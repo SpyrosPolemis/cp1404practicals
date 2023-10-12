0576ac5fa79e3febd3f67ffdd1bd24db1d99840b
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-10-12 22:42:26 +1000

    Align and sort output by alphabetical order

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -12,5 +12,9 @@ for word in words:
         word_to_number_of_word[word] = 1
     else:
         word_to_number_of_word[word] += 1
-for word, number_of_word in word_to_number_of_word.items():
-    print(f"{word} : {number_of_word}")
+words_and_counts = list(word_to_number_of_word.items())
+words_and_counts.sort()
+longest_word_length = max((len(word) for word in word_to_number_of_word))
+for word_and_count in words_and_counts:
+    print(f"{word_and_count[0]:{longest_word_length}} : {word_and_count[1]}")
+
