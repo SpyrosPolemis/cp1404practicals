d617a4ffb1f25ec078677f3017feb651d470df07
SpyrosPolemis <Spyridon.polemis@my.jcu.edu.au>
2023-10-13 11:33:03 +1000

    Write print_countries function to print countries that have won wimbledon

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -15,19 +15,41 @@ with in_file
 def main():
     """ """
     filename = "wimbledon.csv"
-    years = process_file(filename)
-    print(years)
+    records = process_file(filename)
+    print_champions(records)
+    print_countries(records)
+
+
+def print_countries(records):
+    countries = []
+    for record in records:
+        countries.append(record[1])
+    countries = set(countries)
+    print("These twelve countries have won Wimbledon:")
+    print(countries)
+
+
+def print_champions(records):
+    player_to_number_of_wins = {}
+    for record in records:
+        if record[2] in player_to_number_of_wins:
+            player_to_number_of_wins[record[2]] += 1
+        else:
+            player_to_number_of_wins[record[2]] = 1
+    print("Wimbledon Champions:")
+    for player, number_of_wins in player_to_number_of_wins.items():
+        print(f"{player} {number_of_wins}")
 
 
 def process_file(filename):
-    years = []
+    records = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
         for line in in_file:
             line = line.strip()
             # line = line[5:line.find(',', line.find(',', line.find(',') + 1) + 1)]  # for future generations to see
-            years.append(line.split(","))
-    return years
+            records.append(line.split(","))
+    return records
 
 
 main()
