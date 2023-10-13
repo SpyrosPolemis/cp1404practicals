5759028d24005d28eb7b092f6fcab5ac915a6a93
SpyrosPolemis <Spyridon.polemis@my.jcu.edu.au>
2023-10-13 11:06:04 +1000

    Write process_file function which takes file and returns contents of file as a list of lists

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,16 +1,33 @@
 """
 Game, set, match!
 Estimated: 40 mins
-Actual: At least 18 minutes
+Actual: Too long, too long.
 
-for line in file
-    we want tooo
-    keep track of how many times each player appears (dict)
-    each country (set)
+get filename
+with in_file
+    read first line of in_file
+    process_file_in_good_format
+    find_champs
+    find_countries
 """
 
 
 def main():
+    """ """
+    filename = "wimbledon.csv"
+    years = process_file(filename)
+    print(years)
+
+
+def process_file(filename):
+    years = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            line = line.strip()
+            # line = line[5:line.find(',', line.find(',', line.find(',') + 1) + 1)]  # for future generations to see
+            years.append(line.split(","))
+    return years
 
 
 main()
