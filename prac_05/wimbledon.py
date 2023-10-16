11b447dcbd7c747a3baf49ecc3a6655d547af3be
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-10-16 13:51:57 +1000

    Change output to print players by order of number of wins

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -11,21 +11,23 @@ with in_file
     find_countries
 """
 
+from operator import itemgetter
+
 
 def main():
     """Read match history from file and print winning players and countries."""
-    # filename = "wimbledon.csv"
-    filename = input("Enter filename: ")
+    filename = "wimbledon.csv"
+    # filename = input("Enter filename: ")
     match_results = format_match_results(filename)
-    player_to_number_of_wins = count_player_wins(match_results)
-    country_wins = derive_winning_countries(match_results)
+    winning_players = count_player_wins(match_results)
+    winning_countries = derive_winning_countries(match_results)
     # Print winning players
     print("Wimbledon Champions:")
-    for player, number_of_wins in player_to_number_of_wins.items():
-        print(f"{player} {number_of_wins}")
+    for winning_player in winning_players:
+        print(f"{winning_player[0]} {winning_player[1]}")
     # Print winning countries
     print("These twelve countries have won Wimbledon:")
-    print(*country_wins, sep=", ")
+    print(*winning_countries, sep=", ")
 
 
 def derive_winning_countries(match_results):
@@ -45,7 +47,9 @@ def count_player_wins(match_results):
             player_to_number_of_wins[match_result[2]] += 1
         else:
             player_to_number_of_wins[match_result[2]] = 1
-    return player_to_number_of_wins
+    winning_players = list(player_to_number_of_wins.items())
+    winning_players.sort(key=itemgetter(1), reverse=True)
+    return winning_players
 
 
 def format_match_results(filename):
@@ -59,6 +63,7 @@ def format_match_results(filename):
             match_results.append(parts)
             # line = line[5:line.find(',', line.find(',', line.find(',') + 1) + 1)]  # for future generations to see
     return match_results
+    # returns in format: [["1968", "AUS", "Federer", etc.], ["1969", "FRA", "Spyros", etc.]]
 
 
 main()
