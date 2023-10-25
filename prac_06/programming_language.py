18de572119af76b67c12becd06e6b9971edf439a
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-10-25 17:34:39 +1000

    Define __init__ for ProgrammingLanguage class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,12 @@
+"""ProgrammingLanguage class for CP1404 prac week_06"""
+
+class ProgrammingLanguage:
+    """Represent a programming language object"""
+
+    def __init__(self, typing, reflection, year):
+        """Initialise a ProgrammingLanguage instance"""
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
