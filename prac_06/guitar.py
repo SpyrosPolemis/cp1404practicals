ec9a54910500c5aa57ab0c5401eec4e4d75a1e86
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-10-25 18:21:33 +1000

    Define get_age and is_vintage methods for Guitar class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,8 +1,11 @@
 """Guitar class for cp1404 prac_06"""
 
+CURRENT_YEAR = 2023
+
 
 class Guitar:
     """Represent a guitar object"""
+
     def __init__(self, name="", year=0, cost=0):
         """Initialise a guitar instance"""
         self.name = name
@@ -12,4 +15,8 @@ class Guitar:
     def __str__(self):
         return f"{self.name}, ({self.year}) : ${self.cost:,}"
 
+    def get_age(self):
+        return CURRENT_YEAR - self.year
 
+    def is_vintage(self):
+        return self.get_age() > 50
