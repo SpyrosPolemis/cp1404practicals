d87ba5eed15db33caecc72ca7d556705b721b209
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-10-25 19:11:45 +1000

    Add section to print owned guitars and refactor code into get_guitars, print_guitars and test_print_guitars functions

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -7,17 +7,37 @@ from guitar import Guitar
 
 
 def main():
+    """"""
+    guitars = get_guitars()
+    print_guitars(guitars)
+
+
+def print_guitars(guitars):
+    print("These are my guitars:")
+    for i , guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>} ({guitar.year}), worth $ {guitar.cost:,.2f}{vintage_string}")
+
+
+def get_guitars():
     guitars = []
     print("My guitars!")
-    guitar_name = "f"
+    guitar_name = input("Name: ")
     while guitar_name != "":
-        guitar_name = input("Name: ")
         guitar_year = input("Year: ")
         guitar_cost = input("Cost: ")
         new_guitar = Guitar(guitar_name, guitar_year, guitar_cost)
         guitars.append(new_guitar)
         print(new_guitar, "added.")
+        guitar_name = input("Name: ")
+    return guitars
 
 
+def test_print_guitars(guitars):
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    print_guitars(guitars)
 
+
+# test_print_guitars([])
 main()
