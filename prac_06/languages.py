2c6417858b78b065eed3232354634d5896b78efc
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-10-25 17:48:12 +1000

    Create and display instances of ProgrammingLanguage class

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -4,3 +4,10 @@ Actual time:
 
 Client code to use the ProgrammingLanguages class
 """
+
+from programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
