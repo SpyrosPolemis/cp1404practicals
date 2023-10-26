a6184e01987597ca498a93101ffdbdce5ee44307
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-10-26 19:52:03 +1000

    Add docstrings to functions

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -13,6 +13,7 @@ def main():
 
 
 def print_guitars(guitars):
+    """Print all user's guitars."""
     print("These are my guitars:")
     for i, guitar in enumerate(guitars, 1):
         print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth $ {guitar.cost:,.2f}"
@@ -20,6 +21,7 @@ def print_guitars(guitars):
 
 
 def get_guitars():
+    """Get guitars from user until they enter blank string."""
     guitars = []
     print("My guitars!")
     guitar_name = input("Name: ")
@@ -34,6 +36,7 @@ def get_guitars():
 
 
 def test_print_guitars(guitars):
+    """Test print guitars function."""
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
     print_guitars(guitars)
