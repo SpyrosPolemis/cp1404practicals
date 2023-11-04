580d12231a8e7cc6ef74f6130b4fca91e5a1fbb4
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-11-05 01:58:17 +1000

    Add __repr__ method to Project class to use in save_projects_to_file function

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,5 +1,6 @@
 """Project class for CP1404 prac_07"""
 
+
 class Project:
     """Represent a project object."""
 
@@ -11,11 +12,10 @@ class Project:
         self.cost_estimate = float(cost_estimate)
         self.completion_percentage = int(completion_percentage)
 
-
     def __str__(self):
         """Return string representation of Project instance."""
         return f"{self.name}, start:{self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, completion: {self.completion_percentage}%"
 
-
-
-
+    def __repr__(self):
+        """Return string representation of Project instance used for recreating instance."""
+        return f"{self.name}\t{self.start_date}\t{self.priority}\t{self.cost_estimate}\t{self.completion_percentage}"
