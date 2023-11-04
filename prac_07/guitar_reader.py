f7c87f994eee36db842952e7619f3ad57c3e8e86
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-11-04 21:51:58 +1000

    Load guitars from file and make them into Guitar objects, then print them

---
+++ prac_07/guitar_reader.py
@@ -0,0 +1,28 @@
+"""
+Guitar reader program - Reads guitar info from file, then makes Guitar object with each of them and prints them
+
+Estimate: 30
+Actual:
+"""
+from guitar import Guitar
+
+def main():
+    """"""
+    guitars = load_guitars_from_file()
+    for guitar in guitars:
+        print(guitar)
+
+
+def load_guitars_from_file():
+    """Load guitars from file into Guitar objects in as a list."""
+    guitars = []
+    with open("guitars.csv") as in_file:
+        for line in in_file:
+            guitar = line.strip().split(",")
+            guitar = Guitar(guitar[0], guitar[1], guitar[2])
+            guitars.append(guitar)
+    return guitars
+
+
+if __name__ == '__main__':
+    main()
