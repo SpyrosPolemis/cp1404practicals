a15b593af8667bfdf03824f18c18a53cf7afdbe1
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-11-05 13:52:53 +1000

    Add __lt__ method to Project class to allow sorting

--- prac_07/project.py
+++ prac_07/project.py
@@ -14,8 +14,12 @@ class Project:
 
     def __str__(self):
         """Return string representation of Project instance."""
-        return f"{self.name}, start:{self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, completion: {self.completion_percentage}%"
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, completion: {self.completion_percentage}%"
 
     def __repr__(self):
         """Return string representation of Project instance used for recreating instance."""
         return f"{self.name}\t{self.start_date}\t{self.priority}\t{self.cost_estimate}\t{self.completion_percentage}"
+
+    def __lt__(self, other):
+        """Return if object is less than another based on priority."""
+        return self.priority < other.priority
