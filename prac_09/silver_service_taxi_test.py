83f5c19cda8fd3190efc7d3490169e59fd000d9e
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2023-11-19 21:24:27 +1000

    Add rounding for Taxi class

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,6 +1,6 @@
 from silver_service_taxi import SilverServiceTaxi
 
-my_taxi = SilverServiceTaxi("Prius 1", 100, 2)
+my_taxi = SilverServiceTaxi("Cool taxi", 100, 2)
 my_taxi.drive(18)
 print(my_taxi, my_taxi.get_fare())
 my_taxi.start_fare()
